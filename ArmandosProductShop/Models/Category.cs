3b70aa00e71902c90f4853dc89ba8633fa9719e8
Armando Aranda <arandaae19@gmail.com>
2022-08-25 11:18:49 -0500

    Made changes to Product Model. Added properties to use with new Category Model.

---
+++ ArmandosProductShop/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArmandosProductShop.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+
+        [Required(ErrorMessage = "Please enter category name")]
+        public string Name { get; set; }
+    }
+}
--- ArmandosProductShop/Models/Product.cs
+++ ArmandosProductShop/Models/Product.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using ArmandosProductShop.Models;
@@ -10,12 +12,44 @@ namespace ArmandosProductShop.Models
     public class Product
     {
         public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Please enter product name")]
         public string Name {get; set; }
+
+        [Required(ErrorMessage = "Please enter product name")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Please select category")]
+        public int CategoryID { get; set; } //foreign key
+
+        public Category Category { get; set; }
+
+        public decimal DiscountPercent => .20M;
+        public decimal DiscountAmount => Price * DiscountPercent;
+        public decimal DiscountPrice => Price - DiscountAmount;
+
+        [Required(ErrorMessage = "Please enter product code")]
+        public string Code { get; set; }
 
         /// <summary>
         /// This will replace spaces with - in the url for products
         /// </summary>
-        public string Slug => Name.Replace(' ', '-');
+        //public string Slug => Name == null ? "" : Name.Replace(' ', '-');
+
+        public string Slug
+        {
+            get
+            {
+                if (Name == null)
+                {
+                    return "";
+                }
+                else
+                {
+                    return Name.Replace(' ', '-');
+                }
+            }
+        }
     }
 }
