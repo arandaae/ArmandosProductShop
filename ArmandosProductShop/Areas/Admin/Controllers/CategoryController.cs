1511aacea7e93389d8b0c8dd978f50db33a4c46a
Armando Aranda <arandaae19@gmail.com>
2022-08-26 10:16:59 -0500

    Worked on CategoryController in Admin Area

--- ArmandosProductShop/Areas/Admin/Controllers/CategoryController.cs
+++ ArmandosProductShop/Areas/Admin/Controllers/CategoryController.cs
@@ -25,6 +25,78 @@ namespace ArmandosProductShop.Areas.Admin.Controllers
             var categories = context.Categories.OrderBy(c => c.CategoryID).ToList();
             return View(categories);
         }
+        /// <summary>
+        /// Add new category
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            return View("AddUpdate", new Category());
+        }
+
+        /// <summary>
+        /// Update an exsisting category
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            ViewBag.Action = "Update";
+            var category = context.Categories.Find(id);
+            return View("AddUpdate", category);
+        }
+
+        /// <summary>
+        /// Update or add category to database
+        /// </summary>
+        /// <param name="category"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Update(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.CategoryID == 0)
+                {
+                    context.Categories.Add(category);
+                }
+                else
+                {
+                    context.Categories.Update(category);
+                }
+                context.SaveChanges(); //Saves the updated category or Adds a new category to the database
+
+                return RedirectToAction("List", "Category"); //Return to List action inside the Category controller
+            }
+            else
+            {
+                ViewBag.Action = "Save";
+                return View("AddUpdate"); //Return the current view
+            }
+        }
+
+        /// <summary>
+        /// Delete category from the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var category = context.Categories.Find(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Category category)
+        {
+            context.Categories.Remove(category);
+            context.SaveChanges();
+            return RedirectToAction("List", "Category");
+        }
 
     }
 }
